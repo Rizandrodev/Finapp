bcb0e0434f9ecc8029267e86ff1503c31fa48f80
Your Name <you@example.com>
2024-11-29 12:55:25 +0000

    colocando string de conexao com db em variavel de ambiente

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,21 @@
-import express,{Express} from "express"
-import mongoose from "mongoose"
-import FinancialRecordRouter from "./routes/financial-record"
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-record";
+import dotenv from "dotenv"
 
+dotenv.config()
+const app: Express = express();
+const port = process.env.PORT || 3001;
+app.use(express.json());
 
-const app:Express=express()
-const port=process.env.PORT||3000
+const mongoURI: string = process.env.URl_DB="mongodb+srv://carvalhorizandro:0CZo6NLfbh3ItyvM@finapp1.tbxqp.mongodb.net/" || '';
 
-app.use(express.json())
-const mongoURL="mongodb+srv://carvalhorizandro:0CZo6NLfbh3ItyvM@finapp1.tbxqp.mongodb.net"
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("CONNECT TO MONGODB"))
+  .catch((err) => console.error("Failed to Connect to MongoDb ", err));
 
-mongoose.connect(String(mongoURL))
-.then(()=>console.log("connected with Mongodb"))
-.catch((err)=>(console.error("error"+err)))
-
-app.use("/financial-records",FinancialRecordRouter)
-
-app.listen(port,()=>console.log("Server is running"))
+app.use("/finacial-records", financialRecordRouter);
+app.listen(port, () => {
+  console.log(`Server runing on Port ${port}`);
+});
